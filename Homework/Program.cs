7390fca025a7b39a094ee8787a83f8efee4abbde
Dmitrii <razuhindm@gmail.com>
2022-11-08 22:11:42 +0300

    Final homework after course choose specialization

---
+++ Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введи строки в которых надо искать строку длиной менее 3 символов через ,:");
+string startarray = ReadLine()!;
+string[] newarray = startarray.Split(",");
+
+for (int i = 0; i < newarray.Length; i++)
+{
+    if (newarray[i].Length<=3)
+    {
+        Write($"{newarray[i]} ");
+       
+    }
+}
